ced84adf169d2f09229a885453a1be146dffa689
Krushi Raj Tula <krushiraj123@gmail.com>
2025-11-24 23:23:50 +0530

    Add SHA256 hash to Homebrew formula

--- Formula/sshmux.rb
+++ Formula/sshmux.rb
@@ -2,7 +2,7 @@ class Sshmux < Formula
   desc "Persistent SSH sessions using tmux"
   homepage "https://github.com/krushiraj/sshmux"
   url "https://github.com/krushiraj/sshmux/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 ""
+  sha256 "b6f166a2c5581eba5bb4ebbb2cfcc2708640ede41eb3d9420de46514cd1f4bd4"
   license "MIT"
 
   depends_on "tmux"
